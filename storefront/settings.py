89a1afbf4bf87e3702d462456e9d8659403937ef
Daud L Namayala <daudnamayala@gmail.com>
2023-04-27 01:34:34 +0300

    Connecting to mariadb

--- storefront/settings.py
+++ storefront/settings.py
@@ -80,10 +80,14 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "storefront",
+        "USER": "admin",
+        "PASSWORD": "@Davy2130",
+        "HOST": "localhost",
     }
 }
 
