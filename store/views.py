596764e4f145fe9b973e73a12ba7a062b37bcb36
Daud L Namayala <daudnamayala@gmail.com>
2023-04-27 02:06:11 +0300

    Manager and Query set

--- store/views.py
+++ store/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils.translation import gettext as _
+from .models import Product
 # Create your views here.
 
 
 def home(request):
+    query_set = Product.objects.all()
+    for product in query_set:
+        print(product)
     message = _("Hoy es 26 de noviembre.")
     # return HttpResponse(message)
     return render(request, "store/index.html",
