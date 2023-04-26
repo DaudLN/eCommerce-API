e4d19a8fd27968200a2984c84c48fe09ee511d2b
Daud L Namayala <daudnamayala@gmail.com>
2023-04-27 01:54:17 +0300

    Populatig database

--- store/models.py
+++ store/models.py
@@ -28,7 +28,7 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(_("Slug field"))
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
@@ -73,7 +73,7 @@ class OrderItem(models.Model):
     product = models.ForeignKey(Product,  on_delete=models.CASCADE)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField()
-    unity_price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
 
 class Cart(models.Model):
