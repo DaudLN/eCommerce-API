dd95180b9ef0d63bde560a7aefa34244af4d1b16
Daud L Namayala <daudnamayala@gmail.com>
2023-04-27 20:38:25 +0300

    Annotation

--- store/views.py
+++ store/views.py
@@ -42,7 +42,7 @@ def select_related(request):
 def last_five_orders_with_their_customer(request):
     query_set = Order.objects.select_related("customer")\
         .prefetch_related("orderitem_set__product")\
-        .order_by("-placed_at")[:5]
+        .order_by("-placed_at")
     return render(request, "store/orders.html",
                   {"orders": query_set})
 
