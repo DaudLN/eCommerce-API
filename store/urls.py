9ef1a89f34611abcf122e9b941b0db4c938f94ae
Daud L Namayala <daudnamayala@gmail.com>
2023-04-27 14:15:35 +0300

    Filtering

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, re_path
-from .views import home, product
+from .views import home, product, product_that_has_been_ordered
 
 app_name = "store"
 
 urlpatterns = [
     path("", home, name="home"),
-    path("<int:pk>/", product, name="product")
+    path("<int:pk>/", product, name="product"),
+    path("ordered/", product_that_has_been_ordered, name="ordered-product")
 ]
--- store/views.py
+++ store/views.py
@@ -1,12 +1,13 @@
+from django.db.models import Q, F
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.utils.translation import gettext as _
-from .models import Product
+from .models import Product, OrderItem
 # Create your views here.
 
 
 def home(request):
-    query_set = Product.objects.all()
+    query_set = Product.objects.filter(inventory=F('collection__id'))
 
     message = _("Hoy es 26 de noviembre.")
     # return HttpResponse(message)
@@ -19,3 +20,12 @@ def home(request):
 def product(request, pk):
     product = Product.objects.get(pk=pk)
     return render(request, "store/product.html", {"product": product})
+
+
+def product_that_has_been_ordered(request):
+    ordered_qs = OrderItem.objects.values('product__id').distinct()
+    products_ordered_qs = Product.objects.filter(
+        id__in=[ordered_qs]).order_by("title")
+    return render(request, "store/product-ordered.html",
+                  {"product_ordered": products_ordered_qs},
+                  {"total": products_ordered_qs.count()})
