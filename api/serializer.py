e3ceca5c1bd46896a65e195f16d167d631530623
Daud L Namayala <daudnamayala@gmail.com>
2023-04-28 14:27:02 +0300

    Using model serilizer

--- api/serializer.py
+++ api/serializer.py
@@ -1,21 +1,41 @@
 from decimal import Decimal
 from rest_framework import serializers
-from store import models as store
+from store.models import Product, Collection
 
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+# class CollectionSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length=255)
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    unit_price = serializers.DecimalField(6, 2)
+# class ProductSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length=255)
+#     unit_price = serializers.DecimalField(6, 2)
 
+#     price_with_tax = serializers.SerializerMethodField(
+#         method_name="calculate_tax")
+#     collection = CollectionSerializer()
+
+#     def calculate_tax(self, product: store.Product):
+#         return product.unit_price*Decimal(1.1)
+
+
+# Model serializers
+
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+    collection = CollectionSerializer()
     price_with_tax = serializers.SerializerMethodField(
         method_name="calculate_tax")
-    collection = CollectionSerializer()
 
-    def calculate_tax(self, product: store.Product):
+    def calculate_tax(self, product: Product):
         return product.unit_price*Decimal(1.1)
