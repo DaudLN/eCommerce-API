3f611b1658bfdb2e3106d87e11c5c875a08eeb12
Daud L Namayala <daudnamayala@gmail.com>
2023-04-28 10:06:31 +0300

    adding django rest framework

--- storefront/settings.py
+++ storefront/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     "debug_toolbar",
     "store.apps.StoreConfig",
     "store_custom.apps.StoreCustomConfig",
