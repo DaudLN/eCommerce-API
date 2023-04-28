bbd5cb2f7334cf9df8e1f46d53da8204c37b2a8b
Daud L Namayala <daudnamayala@gmail.com>
2023-04-28 14:08:31 +0300

    Serializing relationship

--- api/views.py
+++ api/views.py
@@ -2,20 +2,20 @@ from django.shortcuts import render, get_object_or_404
 from rest_framework.response import Response
 from rest_framework.serializers import Serializer
 from rest_framework.decorators import api_view
-from store import models as store
+from store.models import Product
 from .serializer import ProductSerializer
 # Create your views here.
 
 
 @api_view(['GET', 'POST'])
 def products_list(request):
-    products = store.Product.objects.all()
+    products = Product.objects.select_related("collection").all()
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
 
 
 @api_view(['GET', 'POST'])
 def product(request, pk):
-    product = get_object_or_404(store.Product, pk=pk)
+    product = get_object_or_404(Product, pk=pk)
     serializer = ProductSerializer(product)
     return Response(serializer.data)
